7c716213e3ff51541fca75ccde4e274d0a5b0090
F.Weber <F.Weber@solarlux.de>
2020-08-18 13:11:07 +0200

    Do not catch errors from editUser or editPass function

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,14 @@
 if (!("toJSON" in Error.prototype))
   Object.defineProperty(Error.prototype, "toJSON", {
-    value: function() {
+    value: function () {
       const alt = {};
 
-      Object.getOwnPropertyNames(this).forEach(function(key) {
+      Object.getOwnPropertyNames(this).forEach(function (key) {
         alt[key] = this[key];
       }, this);
 
       return alt;
     },
     configurable: true,
-    writable: true
+    writable: true,
   });
